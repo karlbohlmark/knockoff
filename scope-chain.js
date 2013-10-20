3859659f042d3bfc69317461dc21b50e417a0206
Karl Böhlmark <karl.bohlmark@gmail.com>
2013-10-20 20:14:27 +0200

    better expression support

--- scope-chain.js
+++ scope-chain.js
@@ -8,8 +8,21 @@ function ScopeChain(head, tail) {
 }
 
 ScopeChain.prototype.resolve = function (expr) {
-	var val = staticEval(expr, this.head)
+	var val = staticEval(expr, this)
 	return val !== void(0) ?
 		val:
 		this.tail.resolve(expr)
+}
+
+ScopeChain.prototype.has = function (name) {
+	return (name in this.head) || this.tail && this.tail.has(name)
+}
+
+ScopeChain.prototype.lookup = function (name) {
+	if (name in this.head) {
+		return this.head[name]
+	}
+	if (!this.tail) return void(0)
+
+	return this.tail.lookup(name)
 }
